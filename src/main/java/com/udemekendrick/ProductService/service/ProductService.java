bea1ca95638cbee028db179afadb2b8273798af7
decagon <udemekendrick@gmail.com>
2024-11-14 19:56:32 +0100

    Product service added

---
+++ src/main/java/com/udemekendrick/ProductService/service/ProductService.java
@@ -0,0 +1,4 @@
+package com.udemekendrick.ProductService.service;
+
+public interface ProductService {
+}
---
+++ src/main/java/com/udemekendrick/ProductService/service/ProductServiceImpl.java
@@ -0,0 +1,7 @@
+package com.udemekendrick.ProductService.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductServiceImpl implements ProductService{
+}
